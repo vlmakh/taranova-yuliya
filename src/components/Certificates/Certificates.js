fedafb8a86a484226270428f9aa206e112f2ff0b
Vlad Makhnenko <vlmakh@gmail.com>
2023-03-08 13:52:06 +0200

    add react-animation-on-scroll

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -2,14 +2,21 @@ import { rundark } from "@/images";
 import { CertWrap, CertTitle } from "./Certificates.styled";
 import { Box } from "@/components/Box/Box";
 import Image from "next/image";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 export const Certificates = () => {
   return (
     <CertWrap id="certificates">
       <CertTitle>Certificates</CertTitle>
-      <Box display="flex" justifyContent="center" mt={5}>
-        <Image src={rundark} alt="Run int the dark" width="300" />
-      </Box>
+
+      <AnimationOnScroll
+        animateIn="animate__fadeIn"
+        // animateOnce={true}
+      >
+        <Box display="flex" justifyContent="center" mt={5}>
+          <Image src={rundark} alt="Run int the dark" width="300" />
+        </Box>
+      </AnimationOnScroll>
     </CertWrap>
   );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,18 +1,21 @@
 import { List, Item, ServicesWrap, ServicesTitle } from "./Services.styled";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 export const Services = () => {
   return (
     <ServicesWrap id="services">
       <ServicesTitle>Services</ServicesTitle>
-      <List>
-        <Item>Gym</Item>
-        <Item>Suspension training</Item>
-        <Item>Kettlebell Training</Item>
-        <Item>Stretching</Item>
-        <Item>Aerobic</Item>
-        <Item>Outdoor run</Item>
-        <Item>MSE Training</Item>
-      </List>
+      <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
+        <List>
+          <Item>Gym</Item>
+          <Item>Suspension training</Item>
+          <Item>Kettlebell Training</Item>
+          <Item>Stretching</Item>
+          <Item>Aerobic</Item>
+          <Item>Outdoor run</Item>
+          <Item>MSE Training</Item>
+        </List>
+      </AnimationOnScroll>
     </ServicesWrap>
   );
 };
