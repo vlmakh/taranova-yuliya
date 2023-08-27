d780a19379dacb6f6c779921258709b65c4e1fd7
Vlad Makhnenko <vlmakh@gmail.com>
2023-08-27 09:35:57 +0300

    map certificates

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -3,35 +3,30 @@ import { CertWrap, CertTitle, CertList, CertItem } from "./Certificates.styled";
 import Image from "next/image";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import { useTranslation } from "next-i18next";
+import { ServicesTitle } from "../Services/Services.styled";
 
-export const Certificates = ({lang}) => {
+export const Certificates = ({ lang }) => {
   const { t } = useTranslation("common");
+
+  const certs = [
+    { path: ntc, alt: "NTC", width: 400 },
+    { path: kettle, alt: "Kettlebell Training", width: 400 },
+    { path: cycling, alt: "Indoor Cycling", width: 400 },
+    { path: firstaid, alt: "First Aid", width: 300 },
+  ];
 
   return (
     <CertWrap id="certificates">
-      <CertTitle lang={lang}>{t('cert.title')}</CertTitle>
+      <CertTitle lang={lang}>{t("cert.title")}</CertTitle>
 
       <CertList>
-        <CertItem>
-          <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-            <Image src={ntc} alt="NTC" width="400" />
-          </AnimationOnScroll>
-        </CertItem>
-        <CertItem>
-          <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-            <Image src={kettle} alt="Kettlebell Training" width="400" />
-          </AnimationOnScroll>
-        </CertItem>
-        <CertItem>
-          <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-            <Image src={cycling} alt="Indoor Cycling" width="400" />
-          </AnimationOnScroll>
-        </CertItem>
-        <CertItem>
-          <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-            <Image src={firstaid} alt="First Aid" width="300" />
-          </AnimationOnScroll>
-        </CertItem>
+        {certs.map((cert, idx) => (
+          <CertItem key={idx}>
+            <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
+              <Image src={cert.path} alt={cert.alt} width={cert.width} />
+            </AnimationOnScroll>
+          </CertItem>
+        ))}
       </CertList>
     </CertWrap>
   );
